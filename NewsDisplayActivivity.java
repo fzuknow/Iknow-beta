3a01fdfbfb72c50897df687c72ef7d26cc46055d
sylviareset <1052251409@qq.com>
2017-12-23 01:58:17 +0800

    教务处通知信息获取

---
+++ NewsDisplayActivivity.java
@@ -0,0 +1,25 @@
+package com.example.chen.fzu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by Administrator on 2017/12/22/022.
+ */
+
+public class NewsDisplayActivivity extends AppCompatActivity {
+    private String articleUrl;
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.article_detail);
+        articleUrl = getIntent().getStringExtra("article_Url");
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(articleUrl);
+    }
+
+}
